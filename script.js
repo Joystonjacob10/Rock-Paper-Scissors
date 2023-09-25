156fa52a777b6c9b3ddfc237f11dc7d800d0b2e7
Joystonjacob10 <joystonjacob2614@gmail.com>
2023-09-25 11:46:29 +0530

    Need to create a function to see who wins

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
+function singleRound(playerSelection,getComputerChoice){
+    if (playerSelection.toLowerCase()=="rock" && getComputerChoice=="Paper"){
+        return "You Lose! Paper beats Rock"
+
+    }else if(playerSelection.toLowerCase()=="rock" && getComputerChoice=="Scissors"){
+        return "You Win! Rock beats Scissors"
+    }
+    else if(playerSelection.toLowerCase()=="paper" && getComputerChoice=="Scissors"){
+        return "You Lose! Scissor beats Paper"
+    }else if(playerSelection.toLowerCase()=="paper" && getComputerChoice=="Rock"){
+        return "You Win! Paper beats Rock"
+    }else if(playerSelection.toLowerCase()=="scissor" && getComputerChoice=="Rock"){
+        return "You Lose! Rock beats Scissors"
+    }else if(playerSelection.toLowerCase()=="scissor" && getComputerChoice=="Paper"){
+        return "You Win! Scissor beats Paper"
+    }
+    
+    
+}
